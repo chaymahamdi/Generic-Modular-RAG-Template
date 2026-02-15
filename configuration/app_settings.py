1ba0a48a071a7896c8865cf8948f243f7b77af3c
chaymahamdi <chayma.hamdi@supcom.tn>
2026-02-15 14:47:05 +0100

    add the pgvector configuration as vector database

--- configuration/app_settings.py
+++ configuration/app_settings.py
@@ -8,3 +8,15 @@ class AppSettings(BaseSettings):
     API_VERSION: str = Field(default="v1", alias="API_VERSION")
     PROJECT_NAME: str = Field(alias="PROJECT_NAME", default="General-Purpose-RAG-System")
     NB_WORKERS: int = Field(alias="NB_WORKERS", default=3)
+
+class VectorDatabaseConfig(BaseSettings):
+    """Configuration class for connecting to the PGVector as vector database"""
+    VECTOR_DB_HOST: str = Field(alias='VECTOR_DB_HOST', default='localhost', description="Host for running the vector database :pgvector")
+    VECTOR_DB_PORT: int = Field(alias='VECTOR_DB_PORT', default=5435, description="Port for running the vector database :pgvector")
+    VECTOR_DB_NAME: str = Field(alias='VECTOR_DB_NAME', default='rag_system_vector_db', description="Vector Database name")
+    VECTOR_DB_USER: str = Field(alias='VECTOR_DB_USER', default='rag_system_username', description="Username to access the vector database")
+    VECTOR_DB_PASSWORD: str = Field(alias='VECTOR_DB_PASSWORD', default='rag_system_username', description="password to access the vector database")
+
+    @property
+    def db_url(self):
+        return f"postgresql://{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?user={self.DB_USER}&password={self.DB_PASSWORD}"
