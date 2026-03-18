909914b79573e5717ed09529b01d50231bc3a243
chaymahamdi <99360921+chaymahamdi@users.noreply.github.com>
2026-03-18 01:43:25 +0100

    Revert "Feature/add document data model (#4)"
    This reverts commit 2c3b1865841be23d0ff234f200ac9d0c547e1bc7.

--- src/models/document_table.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String
-
-from src.models.common_models import BaseTable
-
-class DocumentTable(BaseTable):
-    __tablename__ = 'documents'
-    title=Column(String(255), nullable=False)
-    size=Column(String(50))
-    content= Column(String)
