71a852cb612b1ec4ec09be4996d18c56366da1ee
chaymahamdi <99360921+chaymahamdi@users.noreply.github.com>
2026-03-21 14:31:58 +0100

    Feature/add get document by id endpoint (#12)
     add get document by id endpoint, strutucre src project folders

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from uvicorn import Server, Config
 
 from configuration.app_settings import AppSettings
+from configuration.di_container import ServiceDIContainer
 from configuration.logging_configuration import logging_config
 from middlewares.app_middlewares import middlewares
+from src.apis.document_api import document_router
+from src.apis.exception_handlers import register_exception_handlers
 
 app_settings = AppSettings()
 
@@ -19,7 +22,11 @@ def get_application() -> FastAPI:
     return application
 
 app = get_application()
+register_exception_handlers(app)
 
+app.container = ServiceDIContainer()
+
+app.include_router(document_router, tags=["document_api"])
 
 if __name__ == '__main__':
     Server(Config(
