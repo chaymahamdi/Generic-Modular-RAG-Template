560373b8df29665c09c3da4afad0075819d21fa3
chaymahamdi <chayma.hamdi@supcom.tn>
2026-03-21 13:46:45 +0100

    Clean repo from pycache and apply gitignore

--- middlewares/app_middlewares.py
+++
@@ -1,9 +0,0 @@
-from fastapi.middleware import Middleware
-from fastapi.middleware.cors import CORSMiddleware
-
-middlewares = [Middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"])]
